8bcb316f11f95d7c18519db7b70b02b935b5a8ed
ghotane404 <62863408+ghotane404@users.noreply.github.com>
2026-04-08 15:56:36 -0700

    first commit

---
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -0,0 +1,43 @@
+package com.pluralsight;
+
+public class VariableApp {
+    public static void main(String[] args) {
+        // Question 1:
+        // declare variables here
+//      Step 1
+//        Questions:
+//        1. Declare a variable to store your favorite color
+        String favoriteColor = "green";
+//        2. Declare a variable to store the year you started this class
+        int startYear = 2026;
+//        3. Declare a variable to store your middle initial
+        char middleInital = 'S';
+//        4. Declare a variable to store whether you have pets (yes/no)
+        boolean pets = false;
+//        5. Declare a variable with a nice message or saying
+        String message = "You are a cutie pie.";
+
+            // then code solution
+            // then use System.out.println() to display results
+            // ex: System.out.println("The answer is " + answer);
+        System.out.print("My favorite color is " + favoriteColor + ".\n");
+        System.out.print("I started this class in " + startYear + ".\n");
+        System.out.print("My middle initial is " + middleInital + ".\n");
+        System.out.print("Do I have a pet? " + pets + "\n");
+        System.out.println(message);
+
+//      Step 2
+//        1. Declare AND initialize variables for:
+//        2. The number of days in a week
+        int DAYS_PER_WEEK = 7;
+//        3. The price of coffee ($4.99)
+        double price_of_coffee = 4.99f;
+//        4. Your favorite letter
+        char favorite_letter = 'P';
+//        5. Whether it's raining (true/false)
+        boolean weatherRain = false;
+//        6. Print all values in complete sentences
+
+
+    }
+}
